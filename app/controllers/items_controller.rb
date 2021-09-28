888dab5df2d169633ce2b3bce4ca36fd98310740
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-28 15:14:27 -0400

    Created item_params private method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,16 @@ class ItemsController < ApplicationController
         items = Item.all
         render json: items
     end
+
+    def create
+        item = Item.new(item_params)
+    end
+
+
+    private
     
+    def item_params
+        params.require(:item).permit(:name, :price, :description, :image, :category_id)
+    end
+
 end
